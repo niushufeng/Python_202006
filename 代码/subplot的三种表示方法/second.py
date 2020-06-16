8f164abfdf57097a034d8247c2fb9091520606a9
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-06-16 10:03:14 +0800

    Create second.py

---
+++ 代码/subplot的三种表示方法/second.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt 
+import matplotlib.gridspec as gridspec
+
+# method 2:gridspec
+##########################
+plt.figure()
+gs = gridspec.GridSpec(3,3)  # 3行3列
+ax1 = plt.subplot( gs[0,:] )
+ax2 = plt.subplot( gs[1,:2] )
+ax3 = plt.subplot( gs[1:,2] )
+ax4 = plt.subplot( gs[-1,0] )
+ax5 = plt.subplot( gs[-1,-2] )
+
+plt.tight_layout()  # 自动紧凑布局
+plt.show()
