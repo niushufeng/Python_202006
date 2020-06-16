036a3328bf9ac2ffbfbef7c169fe19ef3baeb9c2
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-06-16 14:32:24 +0800

    Update 循环嵌套九九乘法表.py

--- 算法代码/循环嵌套九九乘法表.py
+++ 算法代码/循环嵌套九九乘法表.py
@@ -4,7 +4,7 @@ while row <=9:
     col = 1  # 列号
 
     while col <= row:
-        print("%d * %d = %d" % (col,row,col * row) ,end=" ")  # print自带换行，加一个end=""就不会换行
+        print("%d * %d = %d" % (col,row,col * row) ,end="\t")  # print自带换行，加一个end=""就不会换行,\t是指转义字符，垂直方向对齐
 
         col += 1
     
