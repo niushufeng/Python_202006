bf3ec1158cda8f8f593e9069b79b515a2361f758
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-06-19 16:52:38 +0800

    Create 完整异常语法.py

---
+++ 算法代码/面向对象/异常/完整异常语法.py
@@ -0,0 +1,22 @@
+# 在开发时想要预判到所有的错误，还是有一定的难度
+try:
+    # 1.提示用户输入一个整数
+    num = int(input("输入一个整数："))
+
+    # 2.使用8除以用户输入的整数并且输出
+    result = 8 / num
+
+    print(result)
+
+# except ZeroDivisionError:  # 错误类型1
+#     print("除0错误")   # 针对错误类型1 ，对应的代码处理
+except ValueError:
+    print("请输入正确整数")
+except Exception as result:
+    print("未知错误 %s" % result)
+else:
+    print("尝试成功")
+finally:
+    print("无论是否出现错误都会执行的代码")
+
+print("-" * 50)
