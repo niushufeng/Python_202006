d8ba863ae7ebb36ef70d09c003eb5579da494759
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-06-21 20:42:01 +0800

    Update plan_sprities.py

--- 算法代码/游戏实战演练/飞机大战/框架搭建/plan_sprities.py
+++ 算法代码/游戏实战演练/飞机大战/框架搭建/plan_sprities.py
@@ -2,12 +2,13 @@ import pygame
 
 # 屏幕大小的常量
 SCREEN_RECT = pygame.Rect(0,0,480,700)  # 常量的命名应该所有的字母都使用大写，单词与单词之间用下划线连接
-
+# 刷新的帧率
+FRAM_PRE_SEC = 60
 
 class GameSprite(pygame.sprite.Sprite):
       #   """   飞机大战精灵  """
     
-    def __init__(self,image_name,speed = 1):
+    def __init__(self,image_name,speed=1):
 
         # 调用父类的初始化方法，因为父类不是基于object
         super().__init__()
@@ -21,3 +22,17 @@ class GameSprite(pygame.sprite.Sprite):
 
         # 在屏幕的垂直方向上移动
         self.rect.y += self.speed
+
+
+class Background(GameSprite):
+    # 游戏背景精灵
+
+    def update(self):
+
+        # 1.调用父类的方法实现
+        super().update()
+
+        # 2.判断是否移出屏幕，如果移出屏幕，将图像设置到屏幕的上方
+        if self.rect.y >= SCREEN_RECT.height:
+            self.rect.y = -self.rect.height
+        
