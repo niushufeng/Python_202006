61c011da8eeb347e68d7b4bfedff6ae4159f77e6
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-06-21 16:52:05 +0800

    Create plan_sprities.py

---
+++ 算法代码/游戏实战演练/飞机大战/框架搭建/plan_sprities.py
@@ -0,0 +1,23 @@
+import pygame
+
+# 屏幕大小的常量
+SCREEN_RECT = pygame.Rect(0,0,480,700)  # 常量的命名应该所有的字母都使用大写，单词与单词之间用下划线连接
+
+
+class GameSprite(pygame.sprite.Sprite):
+      #   """   飞机大战精灵  """
+    
+    def __init__(self,image_name,speed = 1):
+
+        # 调用父类的初始化方法，因为父类不是基于object
+        super().__init__()
+
+        # 定义对象的属性
+        self.image = pygame.image.load(image_name)
+        self.rect = self.image.get_rect()  # rect图像的大小 image的get_rect()方法，可以返回pygame.Rect(0,0,图像宽，图像高)的对象
+        self.speed = speed
+
+    def update(self):  # 重写父类update方法
+
+        # 在屏幕的垂直方向上移动
+        self.rect.y += self.speed
