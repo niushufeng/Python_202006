5d3374a6a8d005014fc6d804eafb1aff61a530d1
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-06-21 16:29:01 +0800

    Create plan_main.py

---
+++ 算法代码/游戏实战演练/飞机大战/框架搭建/plan_main.py
@@ -0,0 +1,19 @@
+import pygame
+from plane_sprites import *
+
+
+class PlaneGame(object):
+    # 飞机大战主游戏
+    def __init__(self):
+        print("游戏初始化")
+
+    def start_game(self):
+        print("游戏开始...")
+
+if __name__ == "__main__":  # 对__name__的判断
+    
+    # 创建游戏对象
+    game = PlaneGame()
+
+    # 启动游戏
+    game.start_game()
