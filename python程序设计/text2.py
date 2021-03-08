62a2b2071a2184f5d7b87264a7746990e1a910c2
niushufeng <45967122+niushufeng@users.noreply.github.com>
2021-03-08 15:53:22 +0800

    Create text2.py

---
+++ python程序设计/text2.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  8 15:42:46 2021
+
+@author: niushufeng
+"""
+from math import *
+n = sin(3) # 求正弦值
+print(n)
+ 
+n = gcd(36,18)
+print(n)
+
+n = pi 
+print(n)
+
+n = log2(8)
+print(n)
+
+n = log10(100)
+print(n)
+
+n = radians(180)
+print(n)
