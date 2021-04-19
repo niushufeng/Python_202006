5a2190274b2c4e184076920c9392cb4e834d02df
niushufeng <45967122+niushufeng@users.noreply.github.com>
2021-04-19 15:56:45 +0800

    Create text1.py

---
+++ python程序设计/week8/text1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 19 15:53:23 2021
+
+@author: h3cvdiuser
+"""
+
+#读取CSV文件
+import pandas as pd
+df = pd.read_csv(r'C:\\Users\\h3cvdiuser\\Desktop\\1.csv',engine='python')
+print(df)
