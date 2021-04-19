d10aa07210e30170abb1f219ab30d2e4482cd200
niushufeng <45967122+niushufeng@users.noreply.github.com>
2021-04-19 14:56:50 +0800

    Update text.py

--- python程序设计/week8/text.py
+++ python程序设计/week8/text.py
@@ -23,4 +23,43 @@ def checkdocx(dstStr,fn):
                     return True
                 return False
             
+
+def checkxlsx(dstStr,fn):
+    wb = load_workbook(fn)
+    for ws in wb.worksheets:
+        for row in ws.rows:
+            for cell in rows:
+                try:
+                    if dstStr in cell.valueL:
+                        return
+                    
+def checkpptx(dstStr , fn):
+    for slide in presentation.slides:
+        for shape in slide.shapes:
+            if shape.shape_type  == 19:
+                for row in shape:
+                    
+def main(dstStr, flag):
+    dirs = ['.']
+    while dirs:
+        currentDir = dirs.pop(0)
+        for fn in listdir(currentDir):
+            path = join(currentDir,fn)
+            if isfile(path):
+                if path.endswith('.docx') and checkdocx(dstStr,path):
+                    print(path)
+                elif path.endswith('.xlsx') and checkdocx(dstStr,path):
+                    print(path)
+                elif path.endswith('.pptx') and checkdocx(dstStr,path):
+                    print(path)
+                elif flag and isdir(path):
+                    dirs.append(path)
+            
+
+
+
+
+
+
+            
             
